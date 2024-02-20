db1cd58123a32bbc5310b125afa6ca6caf276ebe
Jakab Botond <jakab.botond@ckik.loc>
2024-02-20 13:02:24 +0200

    juhu

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ function Regful(but)
 {
     but.style.display = "none";
     document.getElementById("login").style.display = "none";
-    document.getElementById("regisztracio").style.display = "block";
+    document.getElementById("regisztracio").style.display = "flex";
+}
+
+function regisztracio()
+{
+    
 }
