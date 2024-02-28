3a5e92181fa3ddd538acac30256834212919e34e
Jakab Botond <jakab.botond@ckik.loc>
2024-02-28 08:04:09 +0100

    belépés felület

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ function Regful(but)
 
 function regisztracio()
 {
+    const regfn = document.getElementById("regFn").value;
+    const regemail = document.getElementById("regemail").value;
+    const regpw = document.getElementById("regPw").value;
+    const regrepw = document.getElementById("regRePw").value;
     
+}
+
+function login()
+{
+    const fn = document.getElementById("fn").value;
+    const pw = document.getElementById("pw").value;
 }
