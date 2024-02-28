1d33122976d835759b5a6ed1c7ed9bfe6081e085
Jakab Botond <jakab.botond@ckik.loc>
2024-02-28 08:27:20 +0100

    asd

--- script.js
+++ script.js
@@ -5,17 +5,36 @@ function Regful(but)
     document.getElementById("regisztracio").style.display = "flex";
 }
 
+async function hash(string) {
+    const utf8 = new TextEncoder().encode(string);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((bytes) => bytes.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
+  }
+
 function regisztracio()
 {
-    const regfn = document.getElementById("regFn").value;
-    const regemail = document.getElementById("regemail").value;
-    const regpw = document.getElementById("regPw").value;
-    const regrepw = document.getElementById("regRePw").value;
+    const regfn = document.getElementById("regFn");
+    const regemail = document.getElementById("regemail");
+    const regpw = document.getElementById("regPw");
+    const regrepw = document.getElementById("regRePw");
     
 }
 
 function login()
 {
-    const fn = document.getElementById("fn").value;
-    const pw = document.getElementById("pw").value;
+    const fn = document.getElementById("fn");
+    const pw = document.getElementById("pw");
+
+    //ha rossz
+    fn.style.border = "solid red 2px";
+    fn.style.boxShadow = "red 1px 1px 4px"
+    fn.style.transition = "ease-in-out .3s";
+    pw.style.border = "solid red 2px";
+    pw.style.boxShadow = "red 1px 1px 4px"
+    pw.style.transition = "ease-in-out .3s";
+
 }
